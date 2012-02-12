e93248f9fb3841adaf3a790d0e9fc6e6e6b8cd1e
Reiner Gerecke <mr.squidd@gmail.com>
2012-02-12 13:45:56 +0100

    render trees and mountains, buggy

--- src/code.js
+++ src/code.js
@@ -1,7 +1,7 @@
 var background_ctx,
 	foreground_ctx,
 	scale = 0.4,
-	texture,
+	base_texture,
 	tile_texture = {
 		1: 'beach-shallow',
 		2: 'shallow-deep',
@@ -11,6 +11,18 @@ var background_ctx,
 		6: 'beach'
 	};
 
+var BUILDINGS = {
+	17: ['trees/as_tupelo1/idle_full/45', 'trees/as_birch/idle_full/45', 'trees/as_maple1/idle_full/45',
+		 'trees/as_tupelo/idle_full/45', 'trees/as_maple2/idle_full/45', 'trees/as_spruce/idle_full/45',
+		 'trees/as_tupelo2/idle_full/45', 'trees/as_maple3/idle_full/45', 'trees/as_spruce1/idle_full/45'],
+	23: ['resources/as_clay/idle/45/1.png'],
+	33: ['fish'],
+	34: ['mountains/as_mountain5x5/idle/45']
+};
+
+var origin = {x: 0, y: document.height / 2},
+	TILE_WIDTH = 64 * scale,
+	TILE_HEIGHT = 32 * scale;
 
 
 $(document).ready(init);
@@ -41,25 +53,64 @@ function init() {
 	background_ctx.fillStyle = "#000";
 	background_ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+	canvas = document.getElementById('foreground');
+	canvas.width = document.width;
+	canvas.height = document.height;
+
+	foreground_ctx = canvas.getContext('2d');
+	foreground_ctx.clearRect(0, 0, canvas.width, canvas.height);
+
 	load_assets();
 }
 
 function load_assets() {
-	texture = new Image();
-	texture.src = 'res/base.png'
-	texture.onload = pre_draw;
+	var loaded = 0;
+	function ready() {
+		loaded++;
+		if (loaded == 2) {
+			pre_draw();
+		}
+	}
+
+	base_texture = new Image();
+	base_texture.src = 'res/base.png'
+	base_texture.onload = ready;
+
+	nature_texture = new Image();
+	nature_texture.src = 'res/nature.png'
+	nature_texture.onload = ready;
 }
 
 function pre_draw() {
 	// draw once
 	draw_water(background_ctx);
-	draw_island(background_ctx);
+	draw_island(background_ctx, map['islands'][0]['grounds']);
+	//draw_grid(foreground_ctx);
+	draw_buildings(foreground_ctx, map['buildings']);
 
 	draw();
 }
 
 function draw() {
 	webkitRequestAnimationFrame(draw);
+}
+
+function draw_grid(ctx) {
+	ctx.strokeStyle = "#000";
+	for (var x = 0; x < document.width; x += TILE_WIDTH) {
+		for (var y = 0; y < document.height; y += TILE_HEIGHT) {
+			ctx.moveTo(origin.x + x,              origin.y + y);
+			ctx.lineTo(origin.x + x + TILE_WIDTH, origin.y + y - TILE_HEIGHT);
+			ctx.moveTo(origin.x + x,              origin.y + y);
+			ctx.lineTo(origin.x + x + TILE_WIDTH, origin.y + y + TILE_HEIGHT);
+
+			ctx.moveTo(origin.x + x,              origin.y - y);
+			ctx.lineTo(origin.x + x + TILE_WIDTH, origin.y - y - TILE_HEIGHT);
+			ctx.moveTo(origin.x + x,              origin.y - y);
+			ctx.lineTo(origin.x + x + TILE_WIDTH, origin.y - y + TILE_HEIGHT);
+		}
+	}
+	ctx.stroke();
 }
 
 function draw_water(ctx) {
@@ -69,27 +120,65 @@ function draw_water(ctx) {
 		for (var j = -1; j < 8; j++) {
 			var x = j * tex.width * scale,
 				y = i * tex.height * scale;
-			ctx.drawImage(texture, tex.xpos, tex.ypos, tex.width, tex.height, x, y, tex.width * scale, tex.height * scale);
+			ctx.drawImage(base_texture, tex.xpos, tex.ypos, tex.width, tex.height, x, y, tex.width * scale, tex.height * scale);
 			x += tex.width * scale / 2;
 			y += tex.height * scale / 2;
-			ctx.drawImage(texture, tex.xpos, tex.ypos, tex.width, tex.height, x, y, tex.width * scale, tex.height * scale);
+			ctx.drawImage(base_texture, tex.xpos, tex.ypos, tex.width, tex.height, x, y, tex.width * scale, tex.height * scale);
 		}
 	}
 }
 
-function draw_island(ctx) {
-	var origin = {x: 50, y: document.height / 2};
+function draw_island(ctx, island) {
+	for (var i = 0, len = island.length; i < len; i++) {
+		var tile_x = island[i][0],
+			tile_y = island[i][1],
+			tile_type = island[i][2];
 
-	for (var i = 0, len = map.length; i < len; i++) {
-		var tile_x = map[i][0],
-			tile_y = map[i][1],
-			tile_type = map[i][2];
+		var tex = sprites[tile_texture[tile_type] + '/' + island[i][3] + '/' + island[i][4]];
 
-		var tex = sprites[tile_texture[tile_type] + '/' + map[i][3] + '/' + map[i][4]];
+		var x = origin.x + (tile_x + tile_y) * TILE_WIDTH / 2,
+			y = origin.y + (tile_y - tile_x) * TILE_HEIGHT / 2 + TILE_HEIGHT / 2;
 
-		var x = origin.x + (tile_x + tile_y) * tex.width / 2 * scale,
-			y = origin.y + (tile_y - tile_x) * tex.height / 2 * scale;
+		ctx.drawImage(base_texture, tex.xpos, tex.ypos, tex.width, tex.height, x, y, TILE_WIDTH, TILE_HEIGHT);
+	}
+}
 
-		ctx.drawImage(texture, tex.xpos, tex.ypos, tex.width, tex.height, x, y, tex.width * scale, tex.height * scale);
+function draw_buildings(ctx, buildings) {
+	for (var i = 0, len = buildings.length; i < len; i++) {
+		var tile_type = buildings[i][0],
+			tile_x = buildings[i][1],
+			tile_y = buildings[i][2];
+
+		var texs = BUILDINGS[tile_type],
+			tex = nature_sprites[texs[Math.floor(Math.random() * texs.length)]];
+
+		// mountains
+		//tile_x -= 3;
+		//tile_y += 3;
+		// trees
+		tile_x -= 1;
+		tile_y += 1;
+
+		if (tex === undefined) {
+			continue;
+		}
+
+		var x = origin.x + (tile_x + tile_y) * TILE_WIDTH / 2,
+			y = origin.y + (tile_y - tile_x) * TILE_HEIGHT / 2 - tex.height * scale + TILE_HEIGHT / 2;
+
+		ctx.drawImage(nature_texture, tex.xpos, tex.ypos, tex.width, tex.height, x, y, tex.width * scale, tex.height * scale);
+
+		/*
+		x = origin.x + (tile_x + tile_y) * TILE_WIDTH / 2;
+		y = origin.y + (tile_y - tile_x) * TILE_HEIGHT / 2;
+		ctx.beginPath();
+		ctx.fillStyle = '#0ff';
+		ctx.moveTo(x,                  y);
+		ctx.lineTo(x + TILE_WIDTH / 2, y - TILE_HEIGHT / 2);
+		ctx.lineTo(x + TILE_WIDTH,     y);
+		ctx.lineTo(x + TILE_WIDTH / 2, y + TILE_HEIGHT / 2);
+		ctx.lineTo(x,                  y);
+		ctx.fill();
+		*/
 	}
 }
--- tools/export_map.py
+++ tools/export_map.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python
 
 import json
+import os
 import sqlite3
 import sys
 
+UH_PATH = '/home/squiddy/projekte/unknown-horizons/'
+
+data = {
+    'islands': []
+}
 
 db = sqlite3.connect(sys.argv[1])
 cursor = db.cursor()
 
-cursor.execute('SELECT * FROM ground')
-data = cursor.fetchall()
+# retrieve buildings
+cursor.execute('SELECT type, x, y FROM building')
+data['buildings'] = cursor.fetchall()
+
+# retrieve islands
+cursor.execute('SELECT x, y, file FROM island')
+for (x, y, filename) in cursor.fetchall():
+    island = {'x': x, 'y': y}
+    island_db = sqlite3.connect(os.path.join(UH_PATH, filename))
+    cursor = island_db.cursor()
+    cursor.execute('SELECT * FROM ground')
+    island['grounds'] = cursor.fetchall()
+    island_db.close()
+    data['islands'].append(island)
+
+
 db.close()
 
 with open(sys.argv[2], 'w') as output:
